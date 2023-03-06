c2dd822dca02c30fe17d929c6f7f8a9e26faf378
Claudio <cditoro@gmail.com>
2023-03-05 23:48:27 -0300

    Retoques

--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -6,7 +6,7 @@ const { Recipe, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const recipe = {
-  name: 'Milanea a la napolitana',
+  title: 'Milanea a la napolitana',
 };
 
 describe('Recipe routes', () => {
