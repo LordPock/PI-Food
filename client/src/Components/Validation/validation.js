8bb24abc46a7b2686e94221a439bf1c187caf945
Claudio <cditoro@gmail.com>
2023-03-13 21:12:29 -0300

    Validations create

--- client/src/Components/Validation/validation.js
+++ client/src/Components/Validation/validation.js
@@ -3,7 +3,7 @@ const regexTitle = /[a-zA-Z0-9]/gi
 export default function validation(receta) {
     var errors = {}
     
-    if (receta.title.length < 5 || receta.title.length > 255) errors.title = 'El título debe contener entre 5 y 255 caracteres'
+    if (receta.title.length <= 5 || receta.title.length >= 255) errors.title = 'El título debe contener entre 5 y 255 caracteres'
     if (!regexTitle.test(receta.title)) errors.title = 'El título no puede contener caracteres especiales'
     if (!receta.title) errors.title = 'El título no puede estar en blanco'
 
@@ -11,9 +11,9 @@ export default function validation(receta) {
 
     if (receta.healthScore < 1 || receta.healthScore > 100) errors.healthScore = 'El HealthScore debe tener un valor entre 1 y 100'
 
-    if (receta.summary.length < 10 || receta.summary.length > 800) errors.summary = 'El resumen debe contener entre 10 y 800 caracteres'
+    if (receta.summary.length <= 10 || receta.summary.length >= 800) errors.summary = 'El resumen debe contener entre 10 y 800 caracteres'
 
-    if (receta.instructions.length < 10 || receta.instructions.length > 800) errors.instructions = 'Las instrucciones deben contener entre 10 y 800 caracteres'
+    if (receta.instructions.length <= 10 || receta.instructions.length >= 800) errors.instructions = 'Las instrucciones deben contener entre 10 y 800 caracteres'
 
     if (!receta.diets) errors.diets = 'Debe elegir al menos un tipo de dieta'
 
